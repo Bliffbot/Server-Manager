580822e73a88bc7f0a5db09627c8bef80e6000a6
Bliffbot <149871105+Bliffbot@users.noreply.github.com>
2024-07-28 14:57:19 +0200

    new project generated

---
+++ src/main/java/org/bliffbot/servermanager/Server_Manager.java
@@ -0,0 +1,17 @@
+package org.bliffbot.servermanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Server_Manager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
